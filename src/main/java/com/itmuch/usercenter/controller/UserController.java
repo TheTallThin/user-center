589b9509207b8b66f0400e374430bacf0a6f6db8
何林冲 <508462026@qq.com>
2022-03-30 11:30:10 +0800

    第二次提交

--- src/main/java/com/itmuch/usercenter/controller/UserController.java
+++ src/main/java/com/itmuch/usercenter/controller/UserController.java
@@ -4,6 +4,7 @@ package com.itmuch.usercenter.controller;
 import com.itmuch.usercenter.pojo.User;
 import com.itmuch.usercenter.service.IUserService;
 import com.itmuch.usercenter.service.impl.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,8 @@ import javax.annotation.Resource;
  * @since 2022-03-06
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Resource
@@ -30,7 +32,24 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryUserById(@PathVariable Integer id){
 
+        log.info("我被请求了");
         return this.iUserService.queryUserById(id);
-
     }
+
+
+
+
+    /***
+     *测试用
+    [user]
+     * @return {@link User}
+     * @throws
+     * @author 何林冲  @date 2022/3/30 10:56
+     */
+    @GetMapping("/q")
+    public User query(User user){
+        return user;
+    }
+
+
 }
